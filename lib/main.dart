a7ad53d4f32f1f820a1e1792dc5b84c892058dec
Dave Emery <davemery2000@gmail.com>
2023-07-16 11:19:41 +0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:whatsapp_clone/barrel/export.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Whatsapp UI',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      home:  const ResponsiveLayout(
+        mobileScreenLayout: MobileLayoutScreen(),
+        webScreenLayout: WebLayoutScreen(),
+      ),
+    );
+  }
+}
