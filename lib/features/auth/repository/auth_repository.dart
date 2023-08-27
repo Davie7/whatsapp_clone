b0e5622a687eddbd897c5a302932330b269402f4
Dave Emery <davemery2000@gmail.com>
2023-08-28 00:34:34 +0300

    Last commit for tonight

--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -1,6 +1,14 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:whatsapp_clone/barrel/export.dart';
+
+final authRepositoryProvider = Provider(
+  (ref) => AuthRepository(
+    auth: FirebaseAuth.instance,
+    firestore: FirebaseFirestore.instance,
+  ),
+);
 
 class AuthRepository {
   final FirebaseAuth auth;
@@ -11,13 +19,20 @@ class AuthRepository {
     required this.firestore,
   });
 
-  void signInWithPhone(BuildContext context, String phoneNumber) async{
+  void signInWithPhone(BuildContext context, String phoneNumber) async {
     try {
-      await auth.verifyPhoneNumber(phoneNumber: phoneNumber, verificationCompleted: (PhoneAuthCredential credential)async{
-        await auth.signInWithCredential(credential);
-      }, verificationFailed: (e){throw Exception(e.message);}, codeSent: ((String verificationId, int? resendToken)async{
-        Navigator.pushNamed(context, OTPScreen.routeName);
-      }), codeAutoRetrievalTimeout: (String verificationId){});
+      await auth.verifyPhoneNumber(
+          phoneNumber: phoneNumber,
+          verificationCompleted: (PhoneAuthCredential credential) async {
+            await auth.signInWithCredential(credential);
+          },
+          verificationFailed: (e) {
+            throw Exception(e.message);
+          },
+          codeSent: ((String verificationId, int? resendToken) async {
+            Navigator.pushNamed(context, OTPScreen.routeName);
+          }),
+          codeAutoRetrievalTimeout: (String verificationId) {});
     } on FirebaseAuthException catch (e) {
       showSnackBar(
         context: context,
